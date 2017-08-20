f49fb61a1b7f29de57b351adc3fabd84df9ce268
kwolter1 <kara.wolter@gmail.com>
2017-08-20 13:53:30 -0500

    setting up services

--- routes/V1Services.js
+++ routes/V1Services.js
@@ -3,9 +3,8 @@ import MediaService from '../services/media.service'
 const express = require('express')
 
 const router = express.Router()
-router.get('/hello', ( req, res ) => {
-  res.json('hello world!')
-})
+
 router.get('/media', MediaService.getMedia)
+
 
 module.exports = router
